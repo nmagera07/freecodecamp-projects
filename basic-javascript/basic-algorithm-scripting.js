1876c59f2b783e6d96f55c6bf9c63698459f35c9
Nate <nmagera07@gmail.com>
2023-05-01 12:41:07 -0400

    finished the basic algorithm scripting course

--- basic-javascript/basic-algorithm-scripting.js
+++ basic-javascript/basic-algorithm-scripting.js
@@ -120,3 +120,124 @@ function truncateString(str, num) {
   
   truncateString("A-tisket a-tasket A green and yellow basket", 8);
 
+
+
+// finders keepers
+
+function findElement(arr, func) {
+  let num = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    num = arr[i]
+    if(func(num)) {
+      return num
+    }
+  }
+    return undefined
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+
+// boo who
+
+function booWho(bool) {
+  return typeof bool === "boolean" ? true : false
+}
+
+booWho(null);
+
+
+// title case a sentence 
+
+function titleCase(str) {
+  let upperCase = str.toUpperCase()
+  let arr = upperCase.split(' ')
+  let final = ''
+  let finalArr = []
+
+
+  for(let i = 0; i < arr.length; i++) {
+    let sliced = arr[i].slice(1).toLowerCase()
+    let first = arr[i][0]
+    final = first + sliced
+    finalArr.push(final)
+  }
+  console.log(finalArr.join(' '))
+  return finalArr.join(' ')
+}
+
+titleCase("I'm a little tea pot");
+
+
+// slice and splice
+
+function frankenSplice(arr1, arr2, n) {
+  let arr = arr2.slice()
+  arr.splice(n,0, ...arr1)
+
+  return arr
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+
+// falsy bouncer
+
+function bouncer(arr) {
+  let filtered = []
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i]) {
+      filtered.push(arr[i])
+    }
+  }
+
+  return filtered
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+
+// where do i belong
+
+function getIndexToIns(arr, num) {
+  let sorted = arr.sort((a,b) => a - b)
+  
+  for(let i = 0; i < sorted.length; i++) {
+    if(sorted[i] >= num) {
+      return i
+    }
+  }
+  return arr.length
+}
+
+getIndexToIns([10,20,30,40,50], 30);
+
+
+function mutation(arr) {
+  let arr1 = arr[1].toLowerCase()
+  let arr2 = arr[0].toLowerCase()
+
+  for(let i = 0; i < arr1.length; i++) {
+    if(arr2.indexOf(arr1[i]) < 0) return false
+  }
+
+  return true
+}
+
+mutation(["hello", "hey"]);
+
+
+// chunky monkey
+
+function chunkArrayInGroups(arr, size) {
+  let newArr = []
+
+  for(let i = 0; i < arr.length; i+= size) {
+    newArr.push(arr.slice(i, i + size))
+  }
+  return newArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
