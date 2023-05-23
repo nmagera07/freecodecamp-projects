7fda2bb986c8bbad5f2d0a186d69ab3015ba5596
Nate <nmagera07@gmail.com>
2023-05-23 12:03:55 -0400

    finishing more of the react course

--- react/basic-react.js
+++ react/basic-react.js
@@ -85,3 +85,150 @@ return (
 
 // the 'app' parent component holds the child components and renders them to the browser
 
+
+// in react, you can pass props, or properties, to child components
+
+<App>
+  <Welcome user='Mark' />
+</App>
+
+const Welcome = (props) => <h1>Hello, {props.user}!</h1>
+
+// the created property 'user' is passed to the component 'Welcome'
+
+// we can use default props in react as well
+
+MyComponent.defaultProps = { location: 'San Francisco' }
+
+
+// react provides useful type-checking features to verify that components receive props of the correct type
+// we can set 'propTypes' on a component to require the data to be a certain type
+// it is considered best practice to set propTypes when you know the type of prop ahead of time
+
+MyComponent.propTypes = { handleClick: PropTypes.func.isRequired }
+
+// this propType checks if 'handleClick' is a function, and it is a required property for this component
+
+
+// one of the most important topics in react is 'state'. 
+// state consists of any data your application needs to know about, that can change over time
+
+this.state = {
+
+}
+
+// you can update state, render it in your UI, or pass it as props to your child components
+
+class StatefulComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    // Only change code below this line
+    this.state = {
+      firstName: 'nate'
+    }
+    // Only change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.firstName}</h1>
+      </div>
+    );
+  }
+};
+
+// react uses a virtual DOM to keep track of changes behind the scenes
+// state is only local to that component, unless you pass it as props to other components
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    // Change code below this line
+    const name = this.state.name
+    // Change code above this line
+    return (
+      <div>
+        { /* Change code below this line */ }
+        <h1>{name}</h1>
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
+
+// react provides a method for updating state called 'setState'. you can call setState like so: 'this.setState()', passing an object
+
+this.setState({
+  username: 'Lewis'
+});
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    // Change code below this line
+    this.setState({
+      name: 'React Rocks!'
+    })
+    // Change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
+
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+    // Change code below this line
+    this.toggleVisibility = this.toggleVisibility.bind(this)
+    // Change code above this line
+  }
+  // Change code below this line
+  toggleVisibility() {
+    this.setState(state => {
+      if (state.visibility === true) {
+         return { visibility: false };
+       } else {
+         return { visibility: true };
+      }
+    });
+  }
+  // Change code above this line
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
+  }
+}
+
